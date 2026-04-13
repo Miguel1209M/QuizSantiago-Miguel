76f213478c58ab0c36e4c094f8f2fecebad5a18a
Santiagh_ <santiago.gutierrezh05@gmail.com>
2026-04-13 11:31:07 -0500

    chore: reset dev to base project

--- src/main/java/org/example/quiz/dto/AuthRequest.java
+++
@@ -1,9 +0,0 @@
-package org.example.quiz.dto;
-
-import lombok.Data;
-
-@Data
-public class AuthRequest {
-    private String username;
-    private String password;
-}
--- src/main/java/org/example/quiz/entity/Role.java
+++
@@ -1,6 +0,0 @@
-package org.example.quiz.entity;
-
-public enum Role {
-    ADMIN,
-    DRIVER
-}
